dbffb08ef5e452de00179ed6fa0adfa471e62d36
Divyank Jain Singhvi <119522879+divyank-jain-singhvi@users.noreply.github.com>
2024-03-29 19:44:40 +0530

    updated backend

--- backend/compiler/models.py
+++ backend/compiler/models.py
@@ -5,8 +5,5 @@ class Code(models.Model):
     lines=models.DecimalField(max_digits=9, decimal_places=2,null=False,blank=False)
     code=models.TextField()
     output=models.TextField()
-    # image=models.ImageField(upload_to='uploade/images',null=False,blank=False)
-    # name=models.CharField(max_length=50,null=False,blank=False)
-
     def __str__(self):
         return self.code
