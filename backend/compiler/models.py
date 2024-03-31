9c1779e4062df3e49331f55bad1e75427e31b9ac
Divyank Jain Singhvi <119522879+divyank-jain-singhvi@users.noreply.github.com>
2024-04-01 01:15:50 +0530

    Delete backend directory
     directry updated

--- backend/compiler/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# added code to send data
-class Code(models.Model):
-    lines=models.DecimalField(max_digits=9, decimal_places=2,null=False,blank=False)
-    code=models.TextField()
-    output=models.TextField()
-    def __str__(self):
-        return self.code
