462d00f7b89482c9f986fce00525512b93152252
divyank-jain-singhvi <you@example.com>
2024-06-11 20:05:45 +0530

    path modified

--- backendserver/settings.py
+++ backendserver/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "backendapp.apps.BackendappConfig",
+    "backendapp.apps.BackendappConfig",
     "rest_framework",
     'corsheaders',
-    'backendapp',
 ]
 
 MIDDLEWARE = [
